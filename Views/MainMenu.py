2259e8aa417cf97e8d0f4934b309a5afb1dd57f2
40CompilerErrors <danvgdesign@gmail.com>
2019-03-12 10:25:44 +0100

    Fixed accidental method call

--- Views/MainMenu.py
+++ Views/MainMenu.py
@@ -16,7 +16,13 @@ class MainMenu(QMainWindow):
         loadUi('./Resources/UI/VentanaInicio.ui', self)
 
         self.setWindowTitle('Pantalla Inicio')
-        self.boton_clasificador.clicked.connect(self.controller.user_access("user",""))
-        self.boton_entrenamiento.clicked.connect(self.controller.user_access("admin",""))
+        self.boton_clasificador.clicked.connect(self.__user_login)
+        self.boton_entrenamiento.clicked.connect(self.__admin_login)
         self.setFixedSize(700,300)
 
+
+    def __user_login(self):
+        self.controller.user_access("user", "")
+
+    def __admin_login(self):
+        self.controller.user_access("admin", "")
