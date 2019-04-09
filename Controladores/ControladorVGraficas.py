2762443cb03df4bb27cb1927e77be14670abccfa
Daniel Vilar <danvgdesign@gmail.com>
2019-04-09 23:24:38 +0200

    Deleted legacy stuff

--- Controladores/ControladorVGraficas.py
+++
@@ -1,8 +0,0 @@
-
-class ControladorVGraficas_class:
-
-    def __init__(self):
-        print("Controlador VentanaGraficas creado...")
-
-    def asignarVentana(self, ventanaGraficas):
-        self.ventanaGraficas=ventanaGraficas
