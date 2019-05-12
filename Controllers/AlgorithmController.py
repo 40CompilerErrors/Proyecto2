7918220e9a6eae933b910d9b2ce59fa3706767da
JavierRelinque <javierrelinque@hotmail.com>
2019-05-12 23:09:46 +0200

    uber control de errores de algoritmos

--- Controllers/AlgorithmController.py
+++ Controllers/AlgorithmController.py
@@ -19,6 +19,8 @@ class AlgorithmController:
         self.errorestimators = False
         self.errorrandom = False
         self.errordepth = False
+        self.errorverbose = False
+        self.erroroob = False
         
         if(self.view.dialog_estimators_RF.text().isdigit()):
             self.webcontroller.n_estimators = int(self.view.dialog_estimators_RF.text())
@@ -45,11 +47,22 @@ class AlgorithmController:
             self.view.label_errors.setText("Fallos en la profundidad máxima")
             self.errordepth = True
             
-        self.webcontroller.verbose = int(self.view.dialog_verbose_RF.text())
+        if(self.view.dialog_verbose_RF.text().isdigit()):
+            self.webcontroller.verbose = int(self.view.dialog_verbose_RF.text())
+            self.errorverbose = False
+        else:
+            self.view.label_errors.setText("Fallos en la verbosidad")
+            self.errorverbose = True
+    
+        if(self.view.dialog_oob_RF.text() == "True" or self.view.dialog_oob_RF.text() == "False"):
+            self.webcontroller.oob_score = bool(self.view.dialog_oob_RF.text())
+            self.erroroob = False
+        else:
+            self.view.label_errors.setText("Fallo a la hora de determinar si usar muestras out-of-bag")
+            self.erroroob = True
+            
         
-        self.webcontroller.oob_score = bool(self.view.dialog_oob_RF.text())
-        
-        if(self.errorestimators == True or self.errordepth == True or self.errorrandom == True):
+        if(self.errorestimators == True or self.errordepth == True or self.errorrandom == True or self.errorverbose == True or self.erroroob == True):
             self.errors = True
         else:
             self.errors = False
@@ -62,12 +75,57 @@ class AlgorithmController:
                 
         
     def naivebayes(self):
-        self.webcontroller.var_smoothing = float(self.view.dialog_NB.text())
-        self.view.close()
+        try:
+            float(self.view.dialog_NB.text())
+            self.webcontroller.var_smoothing = float(self.view.dialog_NB.text())
+            self.view.close()
+        except ValueError:
+            self.view.label_errors.setText("Debe introducir un valor válido de float")
+            self.view.label_errors.setVisible(True)
         
     def svm(self):
-        self.webcontroller.random_state = int(self.view.dialog_random_SVM.text())
-        self.webcontroller.verbose = int(self.view.dialog_verbose_SVM.text())
-        self.webcontroller.shrinking = bool(self.view.dialog_shrinking_SVM.text())
-        self.webcontroller.max_iter = int(self.view.dialog_max_iter_SVM.text())        
-        self.view.close()
+        self.errorrandom = False
+        self.errorverbose = False
+        self.errorshrinking = False
+        self.errormax = False
+        
+        if(self.view.dialog_random_SVM.text().isdigit()):
+            self.webcontroller.random_state = int(self.view.dialog_random_SVM.text())   
+            self.errorrandom = False
+        else:
+            self.view.label_errors.setText("Fallos en el random state")
+            self.errorrandom = True
+
+
+        if(self.view.dialog_verbose_SVM.text().isdigit()):
+            self.webcontroller.verbose = int(self.view.dialog_verbose_SVM.text())
+            self.errorverbose = False
+        else:
+            self.view.label_errors.setText("Fallos en la verbosidad")
+            self.errorverbose = True
+            
+        if(self.view.dialog_shrinking_SVM.text() == "True" or self.view.dialog_shrinking_SVM.text() == "False"):
+            self.webcontroller.shrinking = bool(self.view.dialog_shrinking_SVM.text())
+            self.errorshrinking = False
+        else:
+            self.view.label_errors.setText("Fallo a la hora de determinar si usar heurística shrinking")
+            self.errorshrinking = True    
+        
+        try:
+            int(self.view.dialog_max_iter_SVM.text())
+            self.webcontroller.max_iter = int(self.view.dialog_max_iter_SVM.text())   
+            self.errormax = False
+        except ValueError:
+            self.view.label_errors.setText("Fallos en el máximo número de iteraciones")
+            self.errormax = True
+
+        
+        if(self.errorrandom == True or self.errorverbose == True or self.errorshrinking == True or self.errormax == True):
+            self.errors = True
+        else:
+            self.errors = False
+        
+        if self.errors == False:
+            self.view.close()
+        else:
+            self.view.label_errors.setVisible(True)
