15fd028aaf32d759b3ff749c351af10d01ad017f
Daniel Vilar <danvgdesign@gmail.com>
2019-05-28 23:17:51 +0200

    Download models fixed

--- Model/Model.py
+++ Model/Model.py
@@ -12,14 +12,14 @@ class Model:
 
         db =  DB.DB_Driver()
 
-        for obj in self.bucket.objects.all():
+        for obj in db.bucket.objects.all():
             key = str(obj.key)
             print("Downloading " + key)
             # parent_dir = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
             resources_path = os.path.join(os.getcwd(), 'Resources/Models')
             s3_path = os.path.join(resources_path,key)
 
-            self.s3.meta.client.download_file(DB.BUCKET_NAME,key,s3_path)
+            db.s3.meta.client.download_file(DB.BUCKET_NAME,key,s3_path)
 
         db.closeConnection()
 
